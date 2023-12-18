a67147172c2b6f6c04bd47151e846fc430e33a71
Raiq52 <bastien.touboulie@gmail.com>
2023-12-18 12:16:24 +0100

    Connection ok

---
+++ bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+// bootstrap.php
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
+require_once "vendor/autoload.php";
+
+// Create a simple "default" Doctrine ORM configuration for Attributes
+$config = ORMSetup::createAttributeMetadataConfiguration(
+    array(__DIR__."/src"),
+    true,
+);
+
+$connection = DriverManager::getConnection([
+    'driver'   => 'pdo_mysql',
+    'host'     => 'localhost',
+    'charset'  => 'utf8',
+    'user'     => 'root',
+    'password' => '',
+    'dbname'   => 'kraken',
+], $config);
+
+// obtaining the entity manager
+$entityManager = new EntityManager($connection, $config);
--- cli-config.php
+++ cli-config.php
@@ -1,39 +1,9 @@
 <?php
-// bootstrap.php
-use Doctrine\DBAL\DriverManager;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
+// cli-config.php
+require_once "bootstrap.php";
 
-require_once "vendor/autoload.php";
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($entityManager)
+));
 
-// Create a simple "default" Doctrine ORM configuration for Attributes
-$config = ORMSetup::createAttributeMetadataConfiguration(
-    paths: array(__DIR__."/src"),
-    isDevMode: true,
-);
-// or if you prefer annotation, YAML or XML
-// $config = ORMSetup::createAnnotationMetadataConfiguration(
-//    paths: array(__DIR__."/src"),
-//    isDevMode: true,
-// );
-// $config = ORMSetup::createXMLMetadataConfiguration(
-//    paths: array(__DIR__."/config/xml"),
-//    isDevMode: true,
-//);
-// $config = ORMSetup::createYAMLMetadataConfiguration(
-//    paths: array(__DIR__."/config/yaml"),
-//    isDevMode: true,
-// );
-
-// configuring the database connection
-$connection = DriverManager::getConnection([
-    'driver'   => 'pdo_mysql',
-    'host'     => 'localhost',
-    'charset'  => 'utf8',
-    'user'     => 'root',
-    'password' => '',
-    'dbname'   => 'kraken',
-], $config);
-
-// obtaining the entity manager
-$entityManager = new EntityManager($connection, $config);
+return $helperSet;
